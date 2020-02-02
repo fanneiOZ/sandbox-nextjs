c820ec6f33b26ccd41bcbf206e92cd79f8207009
Panya Batphantana <panya.batphantana@zenrooms.com>
2020-02-02 14:14:07 +0700

    DEVOPS-002 Research with NodeJS build and dockerized

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('default', () => {
+gulp.task('typescript', () => {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest('dist/backend'));
